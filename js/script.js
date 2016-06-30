ac24e82d3271ace104d462f8f1fc4341a6354c5b
Jonathan Carroll <jonathan@toughguymountain.com>
2016-06-30 17:43:31 -0400

    added jquery, added a main image, added weird floating shapes, made a description when you hover over image,

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var height = $(window).height();
+var width = $(window).width();
+var weirdColour;
+
+$(document).ready(function(){
+	$(".weird").each(function()
+		{
+			positionShape(this)
+		});
+	$(".content-item").hover(function() {
+		$(".content-text").fadeIn(500);
+	}, function(){
+		$(".content-text").fadeOut(500);
+	})
+})
+
+function moveShape(shape, position){
+	$(shape).animate({
+    left: "+=" + (width + position)
+  }, 20000,"linear", function() {
+  	positionShape(shape)
+  });
+};
+
+function positionShape(shape){
+	var randomHeight = Math.floor((Math.random() * height));
+	var randomWidth = Math.floor((Math.random() * width) + 100);
+	$(shape).css({top:randomHeight,left:-randomWidth});
+	randomColour();
+	$(".weird").css("background-color",weirdColour);
+	$(".weird").each(function()
+		{
+			moveShape(this,randomWidth)
+		});
+}
+
+function randomColour(){
+	var randColour = Math.floor(Math.random()*3);
+	if (randColour == 0)
+	{
+		weirdColour = "#0133F2";
+	} else if (randColour == 1)
+	{
+		weirdColour = "#89D3E0";
+	} else if (randColour == 2)
+	{
+		weirdColour = "#F9C6D9"
+	}
+}
